83135bfd30083f22cb8588fae697f30fecce7447
GabbereX <[gabberex@mail.ru]>
2023-03-17 01:16:05 +0300

    pre-start preparation 2

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 
+import '@assets/styles/styles.global.scss'
+
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 )
 
 root.render(
 	<StrictMode>
-		<main>
+		<main className='Main'>
 			Hello World
 		</main>
 	</StrictMode>
